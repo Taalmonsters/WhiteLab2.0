fda2060e91bcf66f776e108f40a2379efa48101b
matjemeisje <matje@taalmonsters.nl>
2016-01-17 12:23:22 +0100

    Fix intro.js scrolling bug [iss57]

--- app/assets/javascripts/whitelab.tour.js
+++ app/assets/javascripts/whitelab.tour.js
@@ -2,10 +2,24 @@ Whitelab.tour = {
 	active : false,
 	steps : null,
 	nextUrl : null,
-	intro: null,
+	intro : null,
+	element : null,
+	offset : 0,
 	
 	init : function() {
-		Whitelab.tour.intro = introJs().oncomplete(function() {
+		Whitelab.tour.intro = introJs().onchange(function(target) {
+			var current = this._currentStep;
+			var step = this._options.steps[this._currentStep];
+			if ("element" in step)
+				Whitelab.tour.element = step["element"];
+			else
+				Whitelab.tour.element = null;
+			if ("offset" in step)
+				Whitelab.tour.offset = step["offset"];
+			else
+				Whitelab.tour.offset = 0;
+			Whitelab.tour.setScrollTop();
+		}).oncomplete(function() {
 			if (Whitelab.tour.nextUrl != null)
 				window.location = Whitelab.tour.nextUrl;
 			else 
@@ -20,5 +34,15 @@ Whitelab.tour = {
         });
 
 		Whitelab.tour.intro.start();
+	},
+	
+	setScrollTop : function(current) {
+		if (Whitelab.tour.element != undefined) {
+			console.log("scrolling");
+			var pos = $(Whitelab.tour.element).offset().top - Whitelab.tour.offset - 268;
+			$("body").animate({
+		        scrollTop: pos
+		    }, 200);
+		}
 	}
 };
