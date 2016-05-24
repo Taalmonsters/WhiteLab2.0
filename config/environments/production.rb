d2b68db0b48eb75580aa39c945ee4df50af15377
matjemeisje <matje@taalmonsters.nl>
2016-05-24 15:46:58 +0200

    Bugfix

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -34,22 +34,7 @@ Rails.application.configure do
   # Asset digests allow you to set far-future HTTP expiration dates on all assets,
   # yet still be able to expire them through the digest params.
   config.assets.digest = true
-  config.assets.precompile << Proc.new do |path|
-    if path =~ /\.(css|js|png|svg|ico)\z/
-      full_path = Rails.application.assets.resolve(path).to_s
-      basename = full_path.split(/\//).reverse[0]
-      app_assets_path = Rails.root.join('app', 'assets').to_s
-      if full_path.starts_with?(app_assets_path) && !basename.start_with?('_')
-        puts "including asset: " + full_path
-        true
-      else
-        puts "excluding asset: " + full_path
-        false
-      end
-    else
-      false
-    end
-  end
+  config.assets.precompile += %w[*.png *.jpg *.jpeg *.gif]
 
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
 
