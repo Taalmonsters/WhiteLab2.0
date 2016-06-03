4c62c4b02475939b908e7b5044d7d45ed35b6644
matjemeisje <matje@taalmonsters.nl>
2016-06-03 23:59:08 +0200

    Add search concern for controllers

---
+++ app/controllers/concerns/search.rb
@@ -0,0 +1,14 @@
+module Search
+  extend ActiveSupport::Concern
+  
+  protected
+  
+  # Set current document
+  def set_document
+    patt = params[:patt]
+    patt = @user.search_queries.find(params[:id]).patt if !patt && params.has_key?(:id)
+    @document = Document.new({ :xmlid => params[:xmlid], :patt => patt }) if params.has_key?(:xmlid)
+    @document = nil if @document && @document.token_count.blank?
+  end
+  
+end
