bbd05722e11cc00667c492311b5fbac9a09e0499
matjemeisje <matje@taalmonsters.nl>
2016-06-03 23:58:52 +0200

    Add explore concern for controllers

---
+++ app/controllers/concerns/explore.rb
@@ -0,0 +1,12 @@
+module Explore
+  extend ActiveSupport::Concern
+  
+  protected
+  
+  # Set current document
+  def set_document
+    @document = Document.new({ :xmlid => params[:xmlid] }) if params.has_key?(:xmlid)
+    @document = nil if @document && @document.token_count.blank?
+  end
+  
+end
