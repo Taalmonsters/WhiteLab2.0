3d82000cf04916e92f96f9fb2815f5ac7e3a389d
matjemeisje <matje@taalmonsters.nl>
2016-06-05 13:44:13 +0200

    Add set query method

--- app/controllers/concerns/whitelab_explore.rb
+++ app/controllers/concerns/whitelab_explore.rb
@@ -2,6 +2,7 @@ module WhitelabExplore
   extend ActiveSupport::Concern
 
   included do
+    before_filter :set_query
     before_filter :set_document
     before_filter :set_filter
     before_filter :set_filtered_amount
@@ -13,6 +14,14 @@ module WhitelabExplore
   def set_document
     @document = Document.new({ :xmlid => params[:xmlid] }) if params.has_key?(:xmlid)
     @document = nil if @document && @document.token_count.blank?
+  end
+  
+  # Set current query
+  def set_query
+    if ['ngrams','statistics'].include?(action_name)
+      @query = Explore::Query.find_from_params(action_name, @user.id, query_create_params) if params.has_key?(:patt)
+      @result = @query.execute if @query && !@query.finished? && !@query.failed?
+    end
   end
   
 end
