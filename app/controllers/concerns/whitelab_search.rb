17d543b325595f614bb3f388cbeac74d489cf708
matjemeisje <matje@taalmonsters.nl>
2016-06-05 13:26:03 +0200

    Add missing method

--- app/controllers/concerns/whitelab_search.rb
+++ app/controllers/concerns/whitelab_search.rb
@@ -2,11 +2,19 @@ module WhitelabSearch
   extend ActiveSupport::Concern
 
   included do
+    before_filter :set_query
     before_filter :set_document
+    before_filter :set_filter
     before_filter :set_filtered_amount
   end
   
   protected
+  
+  # Set current query
+  def set_query
+    @query = Search::Query.find_from_params(action_name, @user.id, params) if params.has_key?(:patt)
+    @result = @query.execute if @query && !@query.finished? && !@query.failed?
+  end
   
   # Set current document
   def set_document
