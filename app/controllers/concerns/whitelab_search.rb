7fbb5584d3ff344a57316808e2cb090dd970bd7b
matjemeisje <matje@taalmonsters.nl>
2016-06-05 13:43:08 +0200

    Order methods alphabetically

--- app/controllers/concerns/whitelab_search.rb
+++ app/controllers/concerns/whitelab_search.rb
@@ -10,18 +10,18 @@ module WhitelabSearch
   
   protected
   
-  # Set current query
-  def set_query
-    @query = Search::Query.find_from_params(action_name, @user.id, query_create_params) if params.has_key?(:patt)
-    @result = @query.execute if @query && !@query.finished? && !@query.failed?
-  end
-  
   # Set current document
   def set_document
     patt = params[:patt]
     patt = @user.search_queries.find(params[:id]).patt if !patt && params.has_key?(:id)
     @document = Document.new({ :xmlid => params[:xmlid], :patt => patt }) if params.has_key?(:xmlid)
     @document = nil if @document && @document.token_count.blank?
+  end
+  
+  # Set current query
+  def set_query
+    @query = Search::Query.find_from_params(action_name, @user.id, query_create_params) if params.has_key?(:patt)
+    @result = @query.execute if @query && !@query.finished? && !@query.failed?
   end
   
   private
